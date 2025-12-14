72ffe2942eb3f97ffa23c7269695fa3987d2a7ea
NullPointerZero <Mar-Hoff@mailbox.org>
2025-12-14 17:54:37 +0100

    [FEAT] (udp server) made the listener for udp, should work fine. I also changed the method to shut down the server. WHY: the while True was holding the port open, even after the programm ended, since the it opened a thread, that was listening, and the thread didnt have the possibility to shutdown.

--- server.py
+++ server.py
@@ -4,27 +4,52 @@ import json
 import socket
 import threading
 import sys
+import time
 
-CONFIG_File = "server_config.json"
+CONFIG_File = "config.json"
 
-def load_config:(path):
-    with open(path, 'r', encodings="utf-8") as f:
+def load_config(path):
+    with open(path, 'r', encoding="utf-8") as f:
         return json.load(f)
+
+def udp_server (port, name, stop_event):
+    host = "0.0.0.0"
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((host, port))
+        s.settimeout(1.0)
+
+        print(f"[UDP] {name} hört auf {host}:{port}")
+
+        while not stop_event.is_set():
+            try:
+                data, addr = s.recvfrom(1024)
+            except socket.timeout:
+                    continue
+
+            print(f"[UDP] Paket von {addr} aud {name}: {data!r}")
+            s.sendto(b"OK_UDP: " + data, addr)
+
+    print(f"[UDP] {name} sauber beendet")
 
 
 def main():
-    config_Path = CONFIG_File
+
+    stop_event = threading.Event()
+    threads = []
+
+    config_path = CONFIG_File
     if len(sys.argv) > 1:
         config_path = sys.argv[1]
 
     config = load_config(config_path)
-    listeners = config.get("listen", [])
+    tests = config.get("tests", [])
 
-    if not listeners:
-        print("keine Listener in der Konfiguration definiert.")
+    if not tests:
+        print("keine Tests in der Konfiguration definiert.")
         return
 
-    for entry in listeners:
+    for entry in tests:
         port = entry["port"]
         proto = entry["protocol"].lower()
         name = entry.get("name", f"Port {port}/{proto}")
@@ -32,19 +57,26 @@ def main():
         if proto == "tcp":
             #TODO tcp Logic
             print("tcp will be used here")
-        if proto == "udp":
-            #TODO udp Logic
-            print("udp Logic will be used here")
-        else
-            print("unbekanntes Protokoll in Config: {proto} (Port {port})")
+        elif proto == "udp":
+            t = threading.Thread(
+                target=udp_server,
+                args=(port, name, stop_event)
+                )
+            t.start()
+            threads.append(t)
+        else:
+            print(f"unbekanntes Protokoll in Config: {proto} (Port {port})")
 
     print("Server läuft. Strg + c zum Beenden.")
     try:
         while True:
-            pass
+            time.sleep(1)
     except KeyboardInterrupt:
         print("\n Server wird jetzt beendet")
+        stop_event.set()
 
+    for t in threads:
+        t.join()
 
 if __name__ == "__main__":
     main()
